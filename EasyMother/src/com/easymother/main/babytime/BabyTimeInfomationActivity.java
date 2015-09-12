167ff61b326749a8f93bd5a143ae8903ae59eb82
zaxcler <610529094@qq.com>
2015-09-12 09:44:37 +0800

    9.12.2

--- EasyMother/src/com/easymother/main/babytime/BabyTimeInfomationActivity.java
+++ EasyMother/src/com/easymother/main/babytime/BabyTimeInfomationActivity.java
@@ -196,7 +196,7 @@ public class BabyTimeInfomationActivity extends Activity implements OnClickListe
 						public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 							super.onSuccess(statusCode, headers, response);
 							if (JsonUtils.getRootResult(response).getIsSuccess()) {
-								nannan_birthday.setText(year+"年"+monthOfYear+"月"+dayOfMonth+"日");
+								nannan_birthday.setText(year+"年"+(monthOfYear+1)+"月"+dayOfMonth+"日");
 							}else {
 								Toast.makeText(BabyTimeInfomationActivity.this, "修改失败", Toast.LENGTH_SHORT).show();
 							}
@@ -210,7 +210,7 @@ public class BabyTimeInfomationActivity extends Activity implements OnClickListe
 						}
 					});
 				}
-			}, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.DAY_OF_MONTH));
+			}, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
 			picker.show();
 			break;
 
